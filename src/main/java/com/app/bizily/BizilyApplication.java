1e5004c5fbe22014908fcfcd0b3848fc21bc3263
AlistairTait12 <alistair.tait1@gmail.com>
2021-03-24 15:51:18 +0000

    first commit (really) setup project

---
+++ src/main/java/com/app/bizily/BizilyApplication.java
@@ -0,0 +1,13 @@
+package com.app.bizily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/bizily/BizilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bizily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
