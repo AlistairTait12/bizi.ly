ca2f9004c90017ef458c0f8095ec42ad99c186e3
AlistairTait12 <alistair.tait1@gmail.com>
2021-03-24 17:31:41 +0000

    added form to page which crashes because there are no references to methods

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SignUpForm from "./components/signUpForm.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark bg-dark">ROCK THE NAVBARRR</nav>
+        <SignUpForm />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/frontend/src/components/signUpForm.component.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangeFirstName = this.onChangeFirstName.bind(this);
+    this.onChangeLastName = this.onChangeLastName.bind(this);
+    this.state = {
+      email: "",
+      password: "",
+      firstname: "",
+      lastname: "",
+      submitted: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className="submit-form">
+        {this.state.submitted ? (
+          <div>
+            <h4>Successfully registered!</h4>
+            <button className="btn btn-dark" onClick={this.newUser}>
+              OK
+            </button>
+          </div>
+        ) : (
+          <div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                required
+                value={this.state.email}
+                onChange={this.onChangeEmail}
+                name="email"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                required
+                value={this.state.password}
+                onChange={this.onChangePassword}
+                name="password"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="firstname">First Name</label>
+              <input
+                type="firstname"
+                className="form-control"
+                id="firstname"
+                required
+                value={this.state.firstname}
+                onChange={this.onChangeFirstName}
+                name="firstname"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="lastname">Last Name</label>
+              <input
+                type="lastname"
+                className="form-control"
+                id="lastname"
+                required
+                value={this.state.lastname}
+                onChange={this.onChangeLastName}
+                name="lastname"
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default SignUpForm;
