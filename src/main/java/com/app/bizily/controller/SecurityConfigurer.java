f6e80e3789b048dd65b12a0a977bdeee4a31d248
Graemehedges <graeme.hedges@gmail.com>
2021-03-25 13:56:33 +0000

    user login classes

--- src/main/java/com/app/bizily/controller/SecurityConfigurer.java
+++ src/main/java/com/app/bizily/controller/SecurityConfigurer.java
@@ -1,18 +1,66 @@
 package com.app.bizily.controller;
 
+import com.app.bizily.repository.BizilyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
-    UserDetailsService userDetailsService;
+    BizilyUserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/users");
+    }
+
+//    @Override
+//    protected void configure(final HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/users")
+//                .permitAll()
+//                .and()
+//                .formLogin()
+//                .permitAll()
+//                .and()
+//                .csrf()
+//                .disable();
+//    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/bower_components/**", "/*.js",
+//                        "/*.jsx", "/main.css").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .defaultSuccessUrl("/", true)
+//                .permitAll()
+//                .and()
+//                .httpBasic()
+//                .and()
+//                .csrf().disable()
+//                .logout()
+//                .logoutSuccessUrl("/");
+//    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
