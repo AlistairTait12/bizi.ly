1d562800afbb9d461921e80b554f1e3ff691e5b3
AlistairTait12 <alistair.tait1@gmail.com>
2021-03-25 10:15:08 +0000

    users now persisted in the database

--- src/main/java/com/app/bizily/controller/UserController.java
+++ src/main/java/com/app/bizily/controller/UserController.java
@@ -5,11 +5,9 @@ import com.app.bizily.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class UserController {
