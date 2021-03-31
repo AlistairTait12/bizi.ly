4eed72a431ad063fb7ab13a92687dd2d6d67c94a
Vincenzo Florio <vincenzoflorio31@gmail.com>
2021-03-31 19:41:09 +0100

    Added AddTask2 to make form styling changes

---
+++ src/frontend/src/components/AddTask2.js
@@ -0,0 +1,108 @@
+//imports
+import React from "react";
+import { Formik, Form, useField } from "formik";
+import { TextField, Button } from "@material-ui/core";
+// import { TextField, Button } from "@material-ui/core";
+import * as yup from "yup";
+import UserDataService from "../services/user.service";
+import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
+
+
+
+const useStyles = makeStyles({
+    buttonColor: {
+        backgroundColor: "#FE6B8B",
+        boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+        color: "white",
+        marginTop: 30
+    },
+    inputField: {
+        marginTop: 10
+    }
+});
+//custom hooks/ components
+const MyTextField = ({ placeholder, ...props }) => {
+    const [field, meta] = useField(props);
+    const errorText = meta.error && meta.touched ? meta.error : "";
+    const classes = useStyles();
+
+    return (
+        <TextField
+            label={placeholder}
+            {...field}
+            helperText={errorText}
+            error={!!errorText}
+            variant={"outlined"}
+            className={classes.inputField}
+        />
+    );
+};
+
+//yup validation schema
+const validationSchema = yup.object({
+    firstname: yup.string().required("required"),
+    lastname: yup.string().required("required"),
+    email: yup.string().email("not a valid email").required("required"),
+    password: yup
+        .string()
+        .required("required")
+        .min(10, "Password must be between 10-25 characters")
+        .max(25, "Password must be between 10-25 characters"),
+    confirmPassword: yup
+        .string()
+        .required("required")
+        .oneOf([yup.ref("password"), null], "Passwords must match"),
+});
+
+//form component
+const AddTask = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Formik
+                initialValues={{
+                    email: "",
+                    firstname: "",
+                    lastname: "",
+                    password: "",
+                    confirmPassword: "",
+                }}
+                validationSchema={validationSchema}
+                onSubmit={(data) => {
+                    data.username = data.email;
+                    data.role = ["user"];
+
+                    UserDataService.create(data);
+                }}
+            >
+                {({ values, errors }) => (
+                    <Form>
+                        <div>
+                            <MyTextField
+                                placeholder="Task"
+                                name="task"
+                                type="input"
+                                as={TextField}
+                            ></MyTextField>
+                        </div>
+                        {/*Change to date picker*/}
+                        <div>
+                            <MyTextField
+                                placeholder="Day"
+                                name="day"
+                                type="input"
+                                as={TextField}
+                            ></MyTextField>
+                        </div>
+                        <div>
+                            <Button className={classes.buttonColor} variant="outlined" type="submit">Sign Up</Button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
+};
+
+export default AddTask;
